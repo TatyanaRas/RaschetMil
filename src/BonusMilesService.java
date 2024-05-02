d1cb9f1988866ad4c7470aea95600139cc54c58b
Tatyana <tatyanalipch@yandex.ru>
2024-05-02 20:28:55 +0300

    [200~src/BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,12 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int colvoMil;
+        int numberMil;
         if (price < 20) {
-            colvoMil = 0;
+            numberMil = 0;
         } else {
-            colvoMil = price / 20;
+            numberMil = price / 20;
         }
-        return colvoMil;
+        return numberMil;
 
     }
 }
